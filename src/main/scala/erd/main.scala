83def7ee61fd627ab5a4fdabf4cda0265c10ed30
Ivan Krechetov <ivan.krechetov@sdx.com>
2024-05-29 16:07:13 +0200

    See it running

--- src/main/scala/erd/main.scala
+++ src/main/scala/erd/main.scala
@@ -5,18 +5,18 @@ import akka.actor.typed.{ActorSystem, Behavior}
 import akka.persistence.jdbc.testkit.scaladsl.SchemaUtils
 import com.typesafe.config.ConfigFactory
 
-import scala.concurrent.ExecutionContext
+import java.util.concurrent.TimeUnit
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 object Root {
   def apply(): Behavior[Unit] = Behaviors.setup { context =>
-    given ExecutionContext = context.system.executionContext
-
-    SchemaUtils.createIfNotExists()(context.system.classicSystem).foreach { _ =>
-      val observer = context.spawn(MemorizingEcho(), "observer")
-      context.spawn(Clock(observer), "clock")
-    }
+    Await.result(SchemaUtils.createIfNotExists()(context.system.classicSystem), Duration(1, TimeUnit.SECONDS))
+    val observer = context.spawn(MemorizingEcho(), "observer")
+    context.spawn(Clock(observer), "clock")
     Behaviors.same
   }
 }
 
-@main def run(): Unit = ActorSystem(Root(), "demo", config = ConfigFactory.load("app.conf"))
+@main def run(): Unit =
+  ActorSystem(Root(), "demo", config = ConfigFactory.load("app.conf"))
