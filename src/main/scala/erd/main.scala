27a96f6e5d029320312fddcde3f46de9adf33fcf
Ivan Krechetov <ivan.krechetov@sdx.com>
2024-05-29 12:29:31 +0200

    Ping-pong some messages

--- src/main/scala/erd/main.scala
+++ src/main/scala/erd/main.scala
@@ -1,40 +1,61 @@
 package erd
 
-import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
+import scala.util.{Failure, Success}
 
 object MemorizingEcho {
   final case class Signal(text: String, replyTo: ActorRef[Reply])
   final case class Reply(text: String)
 
   def apply(): Behavior[Signal] = Behaviors.receive { (context, message) =>
-    context.log.debug("Got {}", message.text)
+    context.log.info(s"Got ${message.text}")
     message.replyTo ! Reply(message.text)
     Behaviors.same
   }
 }
 
 object Clock {
-  case object Tick
+  sealed trait Message
+  private case object Tick                                     extends Message
+  private final case class Echo(text: String)                  extends Message
+  private final case class ObservationFailure(message: String) extends Message
 
-  def apply(): Behavior[Tick.type] = setup()
+  def apply(observer: ActorRef[MemorizingEcho.Signal]): Behavior[Message] = setup(observer)
 
-  private def setup(): Behavior[Tick.type] = Behaviors.withTimers { timers =>
-    timers.startTimerWithFixedDelay(Tick, FiniteDuration(4, TimeUnit.SECONDS))
-    receive()
+  private def setup(observer: ActorRef[MemorizingEcho.Signal]): Behavior[Message] = Behaviors.withTimers { timers =>
+    timers.startTimerWithFixedDelay(Tick, FiniteDuration(8, TimeUnit.SECONDS))
+    receive(observer)
   }
 
-  private def receive(): Behavior[Tick.type] = Behaviors.receiveMessage { _ =>
-    Behaviors.same
+  private def receive(observer: ActorRef[MemorizingEcho.Signal]): Behavior[Message] = Behaviors.receive {
+    (context, message) =>
+      message match {
+        case Tick =>
+          given Timeout = Timeout(2, TimeUnit.SECONDS)
+          context.ask[MemorizingEcho.Signal, MemorizingEcho.Reply](
+            observer,
+            me => MemorizingEcho.Signal(System.currentTimeMillis().toString, me)
+          ) {
+            case Success(MemorizingEcho.Reply(message)) => Echo(message)
+            case Failure(exception)                     => ObservationFailure(exception.getMessage)
+          }
+        case Echo(text)                  => context.log.info(s"Echoed: $text")
+        case ObservationFailure(message) => context.log.error(s"UH-OH! $message")
+      }
+      Behaviors.same
   }
 }
 
 object Root {
   def apply(): Behavior[Unit] = Behaviors.setup { context =>
+    val observer = context.spawn(MemorizingEcho(), "observer")
+    val clock    = context.spawn(Clock(observer), "clock")
     Behaviors.same
   }
 }
