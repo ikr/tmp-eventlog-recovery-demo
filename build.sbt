cd23397acd3e9167ac120193f88e14e5833ac85e
Ivan Krechetov <ivan.krechetov@sdx.com>
2024-05-29 10:04:41 +0200

    Start an actor system; add auto-formatting configuration

--- build.sbt
+++ build.sbt
@@ -2,13 +2,15 @@ ThisBuild / version := "0.0.1"
 
 ThisBuild / scalaVersion := "3.4.2"
 
-val AkkaVersion = "2.9.3"
+val AkkaVersion    = "2.9.3"
+val LogbackVersion = "1.5.6"
 
 resolvers += "Akka library repository".at("https://repo.akka.io/maven")
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-persistence-typed" % AkkaVersion,
-  "com.typesafe.akka" %% "akka-persistence-testkit" % AkkaVersion % Test
+  "com.typesafe.akka" %% "akka-persistence-typed"   % AkkaVersion,
+  "com.typesafe.akka" %% "akka-persistence-testkit" % AkkaVersion % Test,
+  "ch.qos.logback"     % "logback-classic"          % LogbackVersion
 )
 
 lazy val root = (project in file("."))
--- src/main/scala/erd/main.scala
+++ src/main/scala/erd/main.scala
@@ -1,3 +1,26 @@
 package erd
 
-@main def run(): Unit = println("Hello, World!")
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+import com.typesafe.config.ConfigFactory
+
+object MemorizingEcho {
+  final case class Request(text: String, replyTo: ActorRef[Response])
+  final case class Response(text: String)
+
+  def apply(): Behavior[Request] = Behaviors.receive { (context, message) =>
+    context.log.debug("Got {}", message.text)
+    message.replyTo ! Response(message.text)
+    Behaviors.same
+  }
+}
+
+object Root {
+  def apply(): Behavior[Unit] = Behaviors.setup { context =>
+    Behaviors.stopped
+  }
+}
+
+@main def run(): Unit = {
+  val system = ActorSystem(Root(), "demo", config = ConfigFactory.load("app.conf"))
+}
